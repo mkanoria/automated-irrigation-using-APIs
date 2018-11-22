7a73bcdf805593f278fe9686622ba6e8bce204bd
mkanoria <mayankkanoria@gmail.com>
2018-11-22 00:12:18 -0500

    Changed script.py

--- script.py
+++ script.py
@@ -1,18 +1,20 @@
-#TODO display the coordinates using geocoding API
-#TODO soil moisture data from Agro API
+# TEST INPUT : 10996 Shoemake Ave, Modesto, CA 95358, USA
 import requests, json, time
-from keys import weather_key, geocoding_key, dark_sky_key
+from keys import weather_key, geocoding_key, dark_sky_key, agro_key, polygon_id
+from datetime import datetime
+from water import water_requirement
 
 # api request format: https://maps.googleapis.com/maps/api/geocode/json?parameters
 
 
 def generate_value(city_name):
+    #Weather API
     # code to generate the url
     API_KEY = weather_key()
     domain = "http://api.openweathermap.org/data/2.5/weather?q="
     url = domain + city_name + "&APPID=" + API_KEY
 
-    #Geocoding
+    #Geocoding API
     url = "https://maps.googleapis.com/maps/api/geocode/json?address="
     GEOCODING_KEY = geocoding_key()
     words = city_name.split()
@@ -32,14 +34,51 @@ def generate_value(city_name):
     request_ds = requests.get(ds_url)
     ds_data = request_ds.json()
     temp = ds_data['currently']['temperature']
-    print(str(temp) + " F")
-    if(temp > 76.6):
-        return 1
-    else:
-        return 0
+    precip = ds_data['daily']['data'][0]['precipIntensityMax'] * 24
+    print("\nThe Temperature at " + str(datetime.now()) + " is " + str(temp) + " F")
 
-    #weather API
+    # change condition for return statement
+    #pass dict into another function?
+    
+    #Using default value for polygon
+    agro_domain = "http://api.agromonitoring.com/agro/1.0/soil?polyid="
+    poly_id = polygon_id()
+    agro_api_key = agro_key()
+    agro_url = agro_domain + poly_id + "&appid=" + agro_api_key
+    request_agro = requests.get(agro_url)
+    agro_data = request_agro.json()
+    soil_moisture = agro_data["moisture"]
+    print("\nMositure is: " + str(soil_moisture*100) + "%")
+
+    if(soil_moisture < 0.30):
+        soil_type = "dry"
+    elif (soil_moisture < 65):
+        soil_type = "moist"
+    else:
+        soil_type = "wet"
+        
+    if(temp > 77):
+        temp_condition = "hot"
+    elif(temp>59):
+        temp_condition = "medium"
+    else:
+        temp_condition = "low"
+
+    WATER_REQUIRED = water_requirement(soil_type, temp_condition)
+
+    WATER_AFTER_PRECIP = WATER_REQUIRED - precip
+
+    print("Water Required is: " + str(WATER_REQUIRED) + " mm")
+    print("Rain predicted is: " + str(precip) + " mm")
+    print("Water Required after rain is: " + str(WATER_AFTER_PRECIP) + " mm")
+
+    if(WATER_AFTER_PRECIP<=0):
+        return 0
+    else:
+        return WATER_AFTER_PRECIP
     '''
+    #weather API
+    
     # code to generate and parse data from API
     request = requests.get(url)
     data = request.json()
@@ -55,23 +94,14 @@ def generate_value(city_name):
     '''
 
 def main():
-    address = input("Enter the address of the place: ")
-    i = 0
-    generate_value(address)
 
+    address = input("Enter the address of the place: ")
+    i = 1
+    
     # decide how many  times to call the API per hour
-    while i < 10: 
+    while i: 
         generate_value(address)
-        i += 1
-        time.sleep(10)
+        time.sleep(43200)
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-    
-
