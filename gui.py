58e9fe389d1affd5666dc9e05b0c8dc37ca906a0
mkanoria <mayankkanoria@gmail.com>
2018-11-22 01:31:39 -0500

    Added hardware_control

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from script import generate_value
+from hardware_control import turned_on
 import sys
+
 def stop_fun():
     root.destroy()
     sys.exit()
@@ -23,6 +25,12 @@ def use_coor():#using coordinates, contains code that opens new window
         mtext=ment.get()
         Label(frame5,text="Entered values are - " + mtext).pack(side=TOP)
         data = generate_value(mtext)
+        Label(frame5,text="Temperature is " + str(data["temp"]) + " F").pack(side=TOP)
+        Label(frame5,text="Moisture is " + str(data["moisture"]) + " mm").pack(side=TOP)
+        Label(frame5,text="Water required for these conditions is: " + str(data["WATER_REQUIRED"]) + " mm").pack(side=TOP)
+        Label(frame5,text="Predicted Rain is: " + str(data["precip"]) + " mm").pack(side=TOP)
+        wat = round(data["WATER_AFTER_PRECIP"], 2)
+        Label(frame5,text="Required water after Rain " + wat + " mm").pack(side=TOP)
          
     #proper format- homebut1=Button(frame1,text="go home shelf 1-linearly",command=home1 ,bg="green",fg="blue")
     root.title("AUTOMATIC GARDEN IRRIGATION SYSTEM")
@@ -61,8 +69,10 @@ def use_add():  #using address, contains code that opens new window
         Label(frame5,text="Moisture is " + str(data["moisture"]) + " mm").pack(side=TOP)
         Label(frame5,text="Water required for these conditions is: " + str(data["WATER_REQUIRED"]) + " mm").pack(side=TOP)
         Label(frame5,text="Predicted Rain is: " + str(data["precip"]) + " mm").pack(side=TOP)
-        Label(frame5,text="Required water after Rain " + str(data["WATER_AFTER_PRECIP"]) + " mm").pack(side=TOP)
-
+        wat = round(data["WATER_AFTER_PRECIP"]+0.5)
+        Label(frame5,text="Required water after Rain " + str(round(data["WATER_AFTER_PRECIP"], 2)) + " mm").pack(side=TOP)
+        turned_on(wat)
+        
     #proper format- homebut1=Button(frame1,text="go home shelf 1-linearly",command=home1 ,bg="green",fg="blue")
     root.title("AUTOMATIC GARDEN IRRIGATION SYSTEM")
     lbl = Label(frame1, text="ADDRESS",font=("Times New Roman", 14))
