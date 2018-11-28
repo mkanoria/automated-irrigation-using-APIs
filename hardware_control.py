a323a88630b79404259e36008d9ffec3772e7c1b
mkanoria <mayankkanoria@gmail.com>
2018-11-28 12:00:19 -0500

    Hardware controls changed

--- hardware_control.py
+++ hardware_control.py
@@ -1,13 +1,13 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
-# GPIO.setup(18,GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
 def turned_off():
     print("Valve Closed")
-    # GPIO.output(18,GPIO.LOW)
+    GPIO.output(18,GPIO.LOW)
 def turned_on(t):
     print("Valve Open for " + str(t) + " seconds ")
-    # GPIO.output(18,GPIO.HIGH)
+    GPIO.output(18,GPIO.HIGH)
     time.sleep(t)
     turned_off()
